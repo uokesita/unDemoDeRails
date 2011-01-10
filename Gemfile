d3f92d9a578181b2300da91f446504834db8d19e
Osledy Bazo <osledybazo@gmail.com>
2011-01-09 22:28:00 -0430

    initial setup

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'haml-rails'
+gem 'jquery-rails'
+gem 'compass', '>=0.10.5'
+gem 'rails3-generators'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Undemoderails::Application.config.secret_token = '36f0f91c663ba553cf79c4b14f0db776e5a49a4def8f67aeaa26da7a9e3a499411e619f33f0b40a763ad400b13d888b82bdc9485ec37496197f2a37c0b0d9e78'
